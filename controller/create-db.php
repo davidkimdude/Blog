d28afe54bc820907735660a147900ac80f3ff37f
David Kim <hhjj0506@gmail.com>
2015-01-29 15:03:39 -0800

    1/29/15

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,5 +1,37 @@
 <?php
-    require_once("../model/database.php");
+    require_once(__DIR__ . "/../model/database.php");
     
     $connection = new mysqli($host, $username, $password);
+    
+    if($connection->connect_error) {
+        die("<p>Error: " . $connection->connect_error . "</p>");
+    }
+    
+    $exists = $connection->select_db($database);
+    
+    if(!$exists) {
+        $query = $connection->query("CREATE DATABASE $database");
+        
+        if($query) {
+            echo "<p>Successfully created a database: " . $database . "</p>";
+        }
+    }
+    else{
+        echo "<p>Database already exists</p>";
+    }
+    
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id))");
+    
+    if($query) {
+        echo "<p>Succesfully created table: posts</p>";
+    }
+    else {
+        echo "<p>$connection->error</p>";
+    }
+    
+    $connection->close();
 
